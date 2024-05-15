7c6d352eeb75e658c131d0c01e788400bbbca303
Mostafa Elhady <mostafaElhady96@github.com>
2024-05-15 09:29:58 +0300

    github ACTIONS

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
+
+  // try to trigger github actions 
   await page.goto('https://playwright.dev/');
+
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
